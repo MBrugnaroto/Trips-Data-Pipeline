284f5d8903827455eebc843181716ef11033582b
mbrugnaroto <brugnaroto@gmail.com>
2022-08-09 02:28:07 -0300

    bugfix: csv data path fix

--- prepare-variables.sh
+++ prepare-variables.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 PATH_AIRFLOW="$(pwd)/env/airflow/"
+PATH_DW="$(pwd)/source/data/statistic_per_vehicle/" 
 
 cd ${PATH_AIRFLOW}
 
-sed -i "s@AIRFLOW_DOCKER_REPORTS=.*@AIRFLOW_DOCKER_REPORTS='${PATH_AIRFLOW}'@" .env
+sed -i "s@AIRFLOW_DOCKER_REPORTS=.*@AIRFLOW_DOCKER_REPORTS='${PATH_DW}'@" .env
 sed -i "s@AIRFLOW_UID=.*@AIRFLOW_UID=$(id -u)@" .env
