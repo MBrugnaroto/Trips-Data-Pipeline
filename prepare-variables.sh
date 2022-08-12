bb4c393908ff474c8b6c9f0f9a7564aac9c24082
mbrugnaroto <brugnaroto@gmail.com>
2022-08-11 23:20:57 -0300

    feat: update path

--- prepare-variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PATH_AIRFLOW="$(pwd)/env/airflow/"
-PATH_DW="$(pwd)/source/data/statistic_per_vehicle/" 
-
-cd ${PATH_AIRFLOW}
-
-sed -i "s@AIRFLOW_DOCKER_REPORTS=.*@AIRFLOW_DOCKER_REPORTS='${PATH_DW}'@" .env
-sed -i "s@AIRFLOW_UID=.*@AIRFLOW_UID=$(id -u)@" .env
