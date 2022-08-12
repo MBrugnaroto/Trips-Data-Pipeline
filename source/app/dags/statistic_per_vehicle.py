e800c789c1a32eced90ab360464abfb898bc16c8
mbrugnaroto <brugnaroto@gmail.com>
2022-08-12 09:50:35 -0300

    feat: modifications to receive environment variables by bash script

--- source/app/dags/statistic_per_vehicle.py
+++ source/app/dags/statistic_per_vehicle.py
@@ -8,40 +8,40 @@ from datetime import datetime
 from docker.types import Mount
 
 ENV = {
-    "DB": Variable.get("DB"),
-    "HOST": Variable.get("HOST"),
-    "PORT": Variable.get("PORT"),
-    "USER": Variable.get("USER"),
-    "PASSWORD": Variable.get("PASSWORD"),
-    "PURL": Variable.get("PURL"),
-    "EADDRESS": Variable.get("EADDRESS"),
-    "EPASSWORD": Variable.get("EPASSWORD"),
-    "EDESTIONATION": "put your destination email here",
-    "AWS_ACCESS_KEY_ID": Variable.get("S3KEY"),
-    "AWS_SECRET_ACCESS_KEY": Variable.get("S3SECRET"),
+    'DB': Variable.get('DB'),
+    'HOST': Variable.get('HOST'),
+    'PORT': Variable.get('PORT'),
+    'USER': Variable.get('USER'),
+    'PASSWORD': Variable.get('PASSWORD'),
+    'PURL': Variable.get('PURL'),
+    'EADDRESS': Variable.get('EADDRESS'),
+    'EPASSWORD': Variable.get('EPASSWORD'),
+    'EDESTIONATION':'prepare_variable_script_will_complete',
+    'AWS_ACCESS_KEY_ID': Variable.get('S3KEY'),
+    'AWS_SECRET_ACCESS_KEY': Variable.get('S3SECRET'),
 }
 
 VOLUME = [
-    Mount(target="/usr/src/app/reports", source=Variable.get("REPORTS"), type="bind")
+    Mount(target='/usr/src/app/reports', source=Variable.get('REPORTS'), type='bind')
 ]
 
 ARGS = {
-    "owner": "root",
-    "depends_on_past": False,
-    "start_date": datetime(2022, 8, 1),
-    "schedule_interval": "@monthly",
+    'owner': 'root',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 8, 1),
+    'schedule_interval': '@monthly',
 }
 
 
-with DAG(dag_id="statistic_per_vehicle", default_args=ARGS, catchup=False) as dag:
+with DAG(dag_id='statistic_per_vehicle', default_args=ARGS, catchup=False) as dag:
     start = BashOperator(
-        task_id="start", bash_command="echo 'Starting the report generation process!!'"
+        task_id='start', bash_command="echo 'Starting the report generation process!!'"
     )
 
     extract = DockerOperator(
-        task_id="extract_vehicle_statistics",
-        image="mbrugnar/extractor_vehicle_statistics:latest",
-        container_name="extractor_vehicle_statistics",
+        task_id='extract_vehicle_statistics',
+        image='mbrugnar/extractor_vehicle_statistics:latest',
+        container_name='extractor_vehicle_statistics',
         command="extractor.py --month \
                                 {{"f"execution_date.strftime('{ '%m' }')""}} \
                               --year \
@@ -49,28 +49,28 @@ with DAG(dag_id="statistic_per_vehicle", default_args=ARGS, catchup=False) as da
         environment=ENV,
         force_pull=True,
         auto_remove=True,
-        network_mode="debezium-compose-network",
+        network_mode='debezium-compose-network',
         mounts=VOLUME,
     )
 
-    with TaskGroup(group_id="process_report") as process_report:
+    with TaskGroup(group_id='process_report') as process_report:
         loader = DockerOperator(
-            task_id="loader_vehicle_statistics",
-            image="mbrugnar/loader_vehicle_statistics:latest",
-            container_name="loader_vehicle_statistics",
+            task_id='loader_vehicle_statistics',
+            image='mbrugnar/loader_vehicle_statistics:latest',
+            container_name='loader_vehicle_statistics',
             command="loader.py --report_date \
                                         {{"f"execution_date.strftime('{ '%m-%Y' }')""}}",
             environment=ENV,
             force_pull=True,
             auto_remove=True,
-            network_mode="analytics-network",
+            network_mode='analytics-network',
             mounts=VOLUME,
         )
 
         senderreport = DockerOperator(
-            task_id="senderreport_vehicle_statistics",
-            image="mbrugnar/senderreport_vehicle_statistics:latest",
-            container_name="senderreport_vehicle_statistics",
+            task_id='senderreport_vehicle_statistics',
+            image='mbrugnar/senderreport_vehicle_statistics:latest',
+            container_name='senderreport_vehicle_statistics',
             command="senderreport.py --month-ref \
                                         {{"f"execution_date.strftime('{ '%B/%Y' }')""}} \
                                      --report_date \
@@ -82,7 +82,7 @@ with DAG(dag_id="statistic_per_vehicle", default_args=ARGS, catchup=False) as da
         )
 
     end = BashOperator(
-        task_id="end", bash_command="echo 'Reporting process completed successfully!!'"
+        task_id='end', bash_command="echo 'Reporting process completed successfully!!'"
     )
 
     start >> extract >> process_report >> end
